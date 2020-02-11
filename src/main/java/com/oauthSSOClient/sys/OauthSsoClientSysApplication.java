484f8dfa6cd305dcdb61f1208fb1f9c0e5cbc0e2
liumiao2000 <liu_2000_112@163.com>
2020-02-11 23:07:37 +0800

    https://www.wandouip.com/t5i212180/

---
+++ src/main/java/com/oauthSSOClient/sys/OauthSsoClientSysApplication.java
@@ -0,0 +1,13 @@
+package com.oauthSSOClient.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthSsoClientSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthSsoClientSysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oauthSSOClient/sys/OauthSsoClientSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oauthSSOClient.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthSsoClientSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
